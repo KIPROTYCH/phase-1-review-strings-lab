4ae9292c40402c45d2e1cf1de5a0a0e6bd219aa9
KIPROTYCH <kiprotych@gmail.com>
2023-06-13 19:14:04 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code in this file!
+// Define the currentUser variable
+const currentUser = "John Smith";
+
+// Generate the reply messages
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
+
+module.exports = {
+    currentUser,
+    welcomeMessage,
+    excitedWelcomeMessage,
+    shortGreeting,
+};
